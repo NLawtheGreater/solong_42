15f282f8d6ba535df029a9aae5fa587588397259
Nicholas Yan Cheuk Law <niclaw@z2t18c4.42bangkok.com>
2023-02-19 02:04:02 +0700

    sdfk

--- so_long/srcs/game_Macos.c
+++ so_long/srcs/game_Macos.c
@@ -40,8 +40,6 @@ void	exit_game(t_data *data, int code)
 	free_sprts_util(data, data->panel.bg);
 	free_sprts_util(data, data->panel.score);
 	mlx_destroy_image(data->mlx, data->player.img.ptr);
-	mlx_clear_window(data->mlx, data->win);
-	mlx_destroy_window(data->mlx, data->win);
 	free_map_tiles(data);
 	if (data->map.filedata)
 		free(data->map.filedata);
@@ -53,23 +51,20 @@ void	error_game(t_data *data, int code, char *msg)
 {
 	if (code == ERROR_FILE_OPEN || code == ERROR_MLX)
 	{
-		ft_putendl_fd("Error: %s\n", 1);
+		ft_putendl_fd("\nError:", 1);
 		ft_putstr_fd(msg, 1);
-		free(data->mlx);
-		data->mlx = NULL;
 		exit(1);
 	}
 	else if (code == ERROR_MAP_INVALID || code == ERROR_IMG)
-		ft_putendl_fd("Error: %s\n", 1);
+	{
+		ft_putstr_fd("Error:", 1);
+		ft_putstr_fd(msg, 1);
+	}
 	else if (code == ERROR_WIN)
 	{
 		ft_putendl_fd("Error: Cannot run MLX WIN\n", 1);
-		mlx_clear_window(data->mlx, data->win);
-		mlx_destroy_window(data->mlx, data->win);
 		free_map_tiles(data);
 	}
-	free(data->mlx);
-	data->mlx = NULL;
 	free(data->map.filedata);
 	exit(1);
 }
